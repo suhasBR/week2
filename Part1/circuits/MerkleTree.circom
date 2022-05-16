0148336beda2f045e381427e937d718a59fb74ad
suhasbr <suhas.bangalore16@gmail.com>
2022-05-17 03:12:03 +0530

    submission zku week2

--- Part1/circuits/MerkleTree.circom
+++ Part1/circuits/MerkleTree.circom
@@ -1,12 +1,56 @@
 pragma circom 2.0.0;
 
 include "../node_modules/circomlib/circuits/poseidon.circom";
+include "../node_modules/circomlib/circuits/switcher.circom";
+
 
 template CheckRoot(n) { // compute the root of a MerkleTree of n Levels 
     signal input leaves[2**n];
     signal output root;
 
     //[assignment] insert your code here to calculate the Merkle root from 2^n leaves
+    
+    //initiate array to store the hashes
+    var len = 2**n;
+    var hash_arr[2**(n+1)-1];
+
+    //copy initial 2**n hashes from leaves signal to hash_arr
+    for(var i=0; i<2**n; i++){
+        hash_arr[i] = leaves[i];
+    }
+
+    /*
+    number of times we would run hash function for an input of 2**n leaves 
+    would be:
+    for a tree with 4 leaves (n=2) ; we would run hash function for 2+1 times
+    for a tree with 8 leaves (n=3) ; we would run hash function for 4+2+1 times
+    .
+    .
+    for a tree with n leaves ; we would run hash function for 2**n-1 times
+    therefore, initialize posiedon hash array with that many elements
+    */
+    var total = 2**n-1;
+    component poseidon[total];
+    var counter = 0;
+
+    //generate merkle tree
+    var offset = 0;
+    
+    while(len>0){//loop stops when len = 0 which is the root
+        for(var i=0;i<len-1;i=i+2){
+            poseidon[counter] = Poseidon(2);
+
+            poseidon[counter].inputs[0] <== hash_arr[offset+i];
+            poseidon[counter].inputs[1] <== hash_arr[offset+i+1];
+            hash_arr[offset+len+(i/2)] <== poseidon[counter].out;
+            counter++;
+        }
+        offset = offset + len;
+        len = len / 2;
+    }
+
+    //last element is the merkle root
+    root <== hash_arr[2**(n+1)-1];
 }
 
 template MerkleTreeInclusionProof(n) {
@@ -16,4 +60,22 @@ template MerkleTreeInclusionProof(n) {
     signal output root; // note that this is an OUTPUT signal
 
     //[assignment] insert your code here to compute the root from a leaf and elements along the path
+
+    component poseidon[n];
+
+    //initiating Switcher to identify whether the node is on the left or right
+    component switching[n];
+
+    for(var i=0;i<n;i++){
+        switching[i] = Switcher();
+        switching[i].L <== i == 0 ? leaf : poseidon[i - 1].out;
+        switching[i].R <== path_elements[i];
+        switching[i].sel <== path_index[i];
+
+        poseidon[i] = Poseidon(2);
+        poseidon[i].inputs[0] <== switching[i].outL;
+        poseidon[i].inputs[1] <== switching[i].outR;
+    }
+
+    root <==  poseidon[n-1].out;
 }
--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -8,13 +8,71 @@ contract MerkleTree is Verifier {
     uint256[] public hashes; // the Merkle tree in flattened array form
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
+    
 
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+         for(uint i=0;i<15;i++){
+            hashes.push(0);
+        }
+
+        hashes[8] = PoseidonT3.poseidon([hashes[0],hashes[1]]);
+        hashes[9] = PoseidonT3.poseidon([hashes[2],hashes[3]]);
+        hashes[10] = PoseidonT3.poseidon([hashes[4],hashes[5]]);
+        hashes[11] = PoseidonT3.poseidon([hashes[6],hashes[7]]);
+
+        hashes[12] = PoseidonT3.poseidon([hashes[8],hashes[9]]);
+        hashes[13] = PoseidonT3.poseidon([hashes[10],hashes[11]]);
+
+        hashes[14] = PoseidonT3.poseidon([hashes[12],hashes[13]]);
+
+        root = hashes[14];
+
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
         // [assignment] insert a hashed leaf into the Merkle tree
+        require(index<8);
+        // hashes[index] = hashedLeaf;
+        // uint256 a = 1;
+        // uint256 b = 2;
+
+        
+        // hashes[8] = PoseidonT3.poseidon([a,b]);
+        // hashes[12] = PoseidonT3.poseidon([hashes[8],hashes[9]]);
+        // hashes[14] = PoseidonT3.poseidon([hashes[12],hashes[13]]);
+        
+        // root = hashes[14];
+
+        // index++;
+
+        
+        //update other elements
+
+        uint next = index;
+
+        if(index%2 == 0){
+            hashes[(index/2)+8] = PoseidonT3.poseidon([hashes[index],hashes[index+1]]);
+            next = (index/2)+8;
+        }
+        else{
+            hashes[((index-1)/2)+8] = PoseidonT3.poseidon([hashes[index],hashes[index-1]]);
+            next = (index-1)/2+8;
+        }
+
+        if(next%2 == 0){
+            hashes[((next-8)/2)+12] = PoseidonT3.poseidon([hashes[next],hashes[next+1]]);
+        }
+        else{
+            hashes[((next-8-1)/2)+12] = PoseidonT3.poseidon([hashes[next],hashes[next-1]]);
+        }
+
+        hashes[14] = PoseidonT3.poseidon([hashes[12],hashes[13]]);
+
+        root = hashes[14];
+
+        index++;
+        return root;
     }
 
     function verify(
@@ -25,5 +83,13 @@ contract MerkleTree is Verifier {
         ) public view returns (bool) {
 
         // [assignment] verify an inclusion proof and check that the proof root matches current root
+
+         
+
+         bool check = verifyProof(a, b, c, input);
+
+         bool same = (input[0] == root);
+
+         return same&&check;
     }
 }
